7d3b840e17c2c173101605c1027ddea70c386c34
Muri <murilo.esker@gmail.com>
2023-06-18 23:48:39 -0300

    feat: add footer structure

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,22 @@
+import Image from 'next/image';
+
 interface Props {}
 
 export function Footer(props: Props) {
   return (
-    <p className="mt-4 text-center text-sm text-dark sm:text-base md:text-base lg:text-base xl:text-base">
-      Copyright © {new Date().getFullYear()} - All rights reserved
-    </p>
+    <div className="flex justify-center items-center space-x-96 mb-6 -mt-4">
+      <p className="text-sm text-dark sm:text-base md:text-base lg:text-base xl:text-base">
+        Copyright © {new Date().getFullYear()} - All rights reserved
+      </p>
+      <div className="flex justify-end items-center w-1440">
+        <Image
+          src="/svgs/CoinSynch.svg"
+          alt="CoinSynch Logo"
+          width={124}
+          height={21}
+          priority
+        />
+      </div>
+    </div>
   );
 }
